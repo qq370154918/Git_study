9a437712480da691cc0b3aff5f987ee49e0bdf0a
qq370154918 <50613644+qq370154918@users.noreply.github.com>
2020-06-17 17:06:11 +0800

    增加test02

---
+++ test02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/6/17 17:03
+# @Author  : liang
+# @File    : test02.py
+# @Software: PyCharm
+
+dic = {
+    "name": "liang",
+    "age": 18,
+    "sex": "男"
+}
